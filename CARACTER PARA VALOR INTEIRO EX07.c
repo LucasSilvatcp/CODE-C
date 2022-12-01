8f1654da2d63a75bbcea66a66a0e73de0c5708b3
LucasSilvatcp <119545488+LucasSilvatcp@users.noreply.github.com>
2022-11-30 21:36:24 -0300

    Add files via upload

---
+++ CARACTER PARA VALOR INTEIRO EX07.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ char a;
+  printf("Digite um caracter e veja oque acontece...");
+  scanf("%c",a);
+  printf("VEJA : %d",a);
+    return 0;
+}
