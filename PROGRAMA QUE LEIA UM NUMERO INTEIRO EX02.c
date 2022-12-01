65f1a72c9f95d7851c01e636fa9e42fd429616fd
LucasSilvatcp <119545488+LucasSilvatcp@users.noreply.github.com>
2022-11-30 21:17:10 -0300

    Add files via upload

---
+++ PROGRAMA QUE LEIA UM NUMERO INTEIRO EX02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int a;
+    printf("Digite um numero:");
+    scanf("%d",&a);
+    printf("\n\nNumero inserido: %d\n\n",a);
+    return 0;
+}
