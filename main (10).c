6ad9ce9dd13b35cfb66ef8e70aef72806444d51d
LucasSilvatcp <119545488+LucasSilvatcp@users.noreply.github.com>
2022-12-23 06:47:34 -0300

    Programa para somar três valores EX 08

---
+++ main (10).c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+ int a, b, c;
+    printf("Digite três valores para somar:");
+    scanf("%d%d%d",&a,&b,&c);
+    printf("A soma dos valores:%d",a+b+c);
+    return 0;
+}
